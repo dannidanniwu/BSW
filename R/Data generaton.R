713330a3f38d537d9f09d0c1b6abbf00f015cea0
danni <dw2625@nyu.edu>
2023-09-01 17:15:40 -0400

    Fit a GAM and also a Bayesian version GAM

--- R/Data generaton.R
+++ R/Data generaton.R
@@ -8,6 +8,7 @@ library(data.table)
 library(mgcv)
 library(lme4)
 library(splines)
+library(brms)
 
 s_define <- function() {
   #cluster-specific intercept
@@ -50,14 +51,45 @@ s_generate <- function(list_of_defs) {
   return(dd) #  generated_data is a data.table
 }
 
-dp <- dd[, .(avg = mean(y)), keyby = .(A, site, k)]
+# dp <- dd[, .(avg = mean(y)), keyby = .(A, site, k)]
+# 
+# ggplot(data = dp, aes(x = k, y = avg)) +
+#   geom_line(aes(group = site, color = factor(A))) +
+#   scale_color_manual(values = c("#a53e3e", "#3ea5a5"),
+#                      guide = guide_legend(reverse = TRUE),
+#                      labels = c("no treatment", "treatment")) +
+#   ylab("average Y") +
+#   xlab("period (k)") +
+#   theme(panel.grid = element_blank(),
+#         legend.title = element_blank()) 
 
-ggplot(data = dp, aes(x = k, y = avg)) +
-  geom_line(aes(group = site, color = factor(A))) +
-  scale_color_manual(values = c("#a53e3e", "#3ea5a5"),
-                     guide = guide_legend(reverse = TRUE),
-                     labels = c("no treatment", "treatment")) +
-  ylab("average Y") +
-  xlab("period (k)") +
-  theme(panel.grid = element_blank(),
-        legend.title = element_blank()) 
+# ggplot(data = dd, aes(x = k, y = y)) +
+#   geom_point(aes(color = factor(A, labels = c("Control", "Intervention"))), 
+#              size = 0.1) +
+#   scale_color_manual(values = c("#d07b7c", "#7ba7d0")) +
+#   facet_wrap(~site, ncol = 8) +
+#   theme(panel.grid = element_blank(),
+#         legend.title = element_blank(),
+#         axis.text.y = element_text(size = 7),
+#         strip.text = element_text(size = 8),
+#         axis.text.x = element_blank(),
+#         axis.ticks.x = element_blank()) +
+#   xlim(c(0, 24)) +
+#   guides(color = guide_legend(override.aes = list(size = 2)))
+
+#Fit a generalized additive model with a site-specific smooth function for time
+list_of_defs <- s_define()
+dd <- s_generate(list_of_defs)
+#fs:random factor smooth interactions
+fitgam <- gam(y ~ A + s(k, site, bs = "fs", k = 12), data = dd)
+#trt effect estimation and standard error
+res_fitgam <- c(summary(fitgam)$p.coeff["A"], summary(fitgam)$se["A"])
+
+#fit a Bayesian version
+#https://fromthebottomoftheheap.net/2018/04/21/fitting-gams-with-brms/
+m2 <- brm(y ~ A +  s(k, site, bs = "fs", k = 12), data = dd, family = gaussian(), seed = 17,
+    iter = 20, warmup = 10,  refresh = 0)
+
+summary(m2)
+msms <- marginal_smooths(m2)
+plot(msms)
