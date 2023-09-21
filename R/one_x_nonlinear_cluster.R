4ed047cb4d908c8c6266aa7c5530c2f85ac9c58c
danni <dw2625@nyu.edu>
2023-09-21 17:56:29 -0400

    Code for big purple

---
+++ R/one_x_nonlinear_cluster.R
@@ -0,0 +1,170 @@
+library(simstudy)
+library(data.table)
+library(mgcv)
+library(lme4)
+library(splines)
+library(brms)
+library(cmdstanr)
+library(slurmR)
+library(dplyr)
+set_cmdstan_path(path = "/gpfs/share/apps/cmdstan/2.25.0")
+# Compile the Stan model
+mod <- cmdstan_model("/gpfs/home/dw2625/r/BS/one_x_nonlinear_penalty.stan")
+
+s_define <- function() {
+  #x
+  def <- defData(varname = "x1", formula = 0, variance = 2)
+  def <- defData(def, varname = "A", formula = 0.5, dist = "binary")
+  #y
+  defOut <- defDataAdd(varname = "y", formula = "exp(-(x1 - 0.5)^2)  + 5 * A", variance = 0.25)
+  #exp(-(u - 0.5)^2) 
+  return(list(def = def, defOut = defOut)) 
+}
+
+s_generate <- function(list_of_defs) {
+  
+  list2env(list_of_defs, envir = environment())
+  
+  #--- add data generation code ---#
+  #24 sites in total
+  ds <- genData(200, def)
+  ds <- addColumns(defOut, ds)
+  #summary(ds)
+  ds[] #  generated_data is a data.table
+}
+
+s_model <- function(train_data, test_data, mod) {
+  #set_cmdstan_path(path = "/gpfs/share/apps/cmdstan/2.25.0")
+  #######Fitting the GAM model with default penalization 
+  fitgam <- gam(y ~ A + s(x1,bs="bs",k=9+4) -1, data = train_data, method="REML")
+  res_fitgam <- c(summary(fitgam)$p.coeff["A"], summary(fitgam)$se["A"])
+  range <-   res_fitgam[1] + c(-1,1) * 1.96 *   res_fitgam[2]
+  
+  # Define the knots based on the training data
+  knots_train <- quantile(train_data$x1, probs=seq(0, 1, length=11)[-c(1, 11)])
+  
+  B_train <- predict(bs(train_data$x1, degree=3, knots=quantile(train_data$x1, probs=seq(0, 1, length=11)[-c(1, 11)])))
+  colnames(B_train) <- paste0("Bspline_", 1:ncol(B_train))
+  
+  # Compute the B-spline basis for the test set using the same knots from the training data
+  B_test_tr <- predict(bs(test_data$x1, degree=3, knots=knots_train))
+  colnames(B_test_tr) <- paste0("Bspline_", 1:ncol(B_test_tr))
+  
+  stan_data <- list(num_data = nrow(train_data),
+                    num_basis = ncol(B_train),
+                    B = t(B_train),
+                    A = train_data$A,
+                    y = train_data$y,
+                    num_data_test = nrow(test_data),
+                    B_test = t(B_test_tr),
+                    A_test = test_data$A)
+  
+  # Fit the Bayesian model
+  fit <- mod$sample(data = stan_data,
+                    refresh = 0,
+                    chains = 4L,
+                    parallel_chains = 4L,
+                    iter_warmup = 500,
+                    iter_sampling = 2500,
+                    show_messages = FALSE)
+  
+  diagnostics_df <- as_draws_df(fit$sampler_diagnostics())
+  div <- sum(diagnostics_df[, 'divergent__'])
+  bayes_gam = fit$summary(variables="beta_A",
+                          posterior::default_summary_measures()[1:3],
+                          quantiles = ~ quantile(., probs = c(0.025, 0.975)),
+                          posterior::default_convergence_measures())
+  covered_bayes =   (bayes_gam$`2.5%`< 5 & 5 < bayes_gam$`97.5%`)
+  
+  #Fit a frequentist linear model with the same basis as the Bayesian model, but no penalization
+  # Incorporating the B-spline basis into the data
+  ds_with_bspline <- cbind(train_data,  B_train)
+  # Fitting the model using gam
+  bspline_terms <- paste(colnames(B_train), collapse = " + ")
+  formula_str <- paste("y ~ A +", bspline_terms, "- 1")
+  fitgam2 <- gam(as.formula(formula_str), data = ds_with_bspline, method="REML")
+  res_fitgam2 <- c(summary(fitgam2)$p.coeff["A"], summary(fitgam2)$se["A"])
+  range2 <-   res_fitgam2[1] + c(-1,1) * 1.96 *   res_fitgam2[2]
+  
+  
+  
+  ##############Test RMSE on the test data#################
+  # Use the GAM model to predict on the test data to get the mean predictions.
+  test_data$pred_gam <- predict(fitgam, newdata = test_data)
+  
+  
+  #posterior_samples <- data.table(as_draws_df(fit$draws()))
+  #a <- as.matrix(posterior_samples%>%select(paste0("a","[",1:length(knots_train)+3,"]")))
+  #beta_A <- as.matrix(posterior_samples%>%select(paste0("beta_A")))
+  
+  posterior_predictions <-fit$summary(variables="y_pred_test",posterior::default_summary_measures()[1])
+  test_data$pred_bayesian <- posterior_predictions$mean
+  
+  # Use the GAM nonpenalizaed version model to predict on the test data to get the mean predictions.
+  ds_with_bspline_test <- cbind(test_data,B_test_tr)
+  test_data$pred_gam_np <- predict(fitgam2, newdata = ds_with_bspline_test)
+  
+  # Calculate prediction error for all models
+  test_data$error_gam <- (test_data$y - test_data$pred_gam)^2
+  test_data$error_bayesian <- (test_data$y - test_data$pred_bayesian)^2
+  test_data$error_gam_np <- (test_data$y - test_data$pred_gam_np)^2
+  
+  # Calculate RMSE for both models
+  rmse_gam <- sqrt(mean(test_data$error_gam))
+  rmse_bayesian <- sqrt(mean(test_data$error_bayesian))
+  rmse_gam_np <- sqrt(mean(test_data$error_gam_np))
+  
+  model_results <- data.table(est_gam_freq= res_fitgam[1], se_gam_freq=res_fitgam[2], 
+                              gam_lowci=range[1], gam_upci=range[2], bayes_gam,div, 
+                              est_gam_freq_np = res_fitgam2[1], se_gam_freq_np=res_fitgam2[2], 
+                              gam_np_lowci=range2[1], gam_np_upci=range2[2],
+                              covered_gam_freq=(range[1] < 5 & 5 < range[2]),
+                              covered_bayes,covered_gam_np_freq=(range2[1] < 5 & 5 < range2[2]),
+                              rmse_gam, rmse_bayesian, rmse_gam_np) %>%
+    mutate(across(-c(variable,covered_gam_freq, covered_bayes,covered_gam_np_freq), round, 3))
+  
+  setnames(model_results, c("est_gam_freq","se_gam_freq","lowci_freq", "upci_freq","variable","est_mean_bayes",
+                            "est_med_bayes","est_sd_bayes",
+                            "lowci_bayes",
+                            "upci_bayes","rhat","ess_bulk","ess_tail",
+                            "div","est_gam_np_freq","se_gam_np_freq","lowci_freq_np", "upci_freq_np",
+                            "covered_freq","covered_bayes","covered_gam_np_freq",
+                            "rmse_gam","rmse_bayesian","rmse_gam_np"))
+  
+  return(model_results)
+}
+
+s_single_rep <- function(list_of_defs, mod) {
+  
+  train_data <- s_generate(list_of_defs)
+  test_data <- s_generate(list_of_defs)
+  model_results <- s_model(train_data, test_data, mod)
+  
+  return(model_results)
+}
+
+s_replicate <- function(iter, mod) {
+  list_of_defs = s_define()
+  model_results = s_single_rep(list_of_defs, mod)
+  return(data.table(iter=iter, model_results))
+}
+
+
+#job <- lapply(1:20,function(i) s_replicate(iter=i, mod=mod))
+sjob <- Slurm_lapply(1:1000,
+                     FUN=s_replicate,
+                     mod=mod,
+                     njobs = 90,
+                     tmp_path = "/gpfs/scratch/dw2625",
+                     job_name = "BS_100",
+                     sbatch_opt = list(time = "12:00:00",partition = "cpu_short", `mem-per-cpu` = "25G"),
+                     export = c("s_define","s_generate","s_model","s_single_rep"),
+                     plan = "wait",
+                     overwrite=TRUE)
+res <- Slurm_collect(sjob) # data is a list
+#res<- site_plasma_all[lapply(site_plasma_all, function(x) length(x))>1] #filter out the error message
+res <- rbindlist(res) # converting list to data.table
+
+date_stamp <- gsub("-", "", Sys.Date())
+dir.create(file.path("/gpfs/home/dw2625/r/BS/", date_stamp), showWarnings = FALSE)
+save(res, file = paste0("/gpfs/home/dw2625/r/BS/", date_stamp, "/one_x_nonlinear.rda"))
