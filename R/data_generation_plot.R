a99eab944c5c0e3de00ec58d064a7a1df83f84a6
Danni Wu <dw2625@nyu.edu>
2023-09-24 20:20:29 -0400

    plot data generation

---
+++ R/data_generation_plot.R
@@ -0,0 +1,25 @@
+dp <- dd[, .(avg = mean(y)), keyby = .(A, site, normk)]
+
+ggplot(data = dp, aes(x = normk, y = avg)) +
+  geom_line(aes(group = site, color = factor(A))) +
+  scale_color_manual(values = c("#a53e3e", "#3ea5a5"),
+                     guide = guide_legend(reverse = TRUE),
+                     labels = c("no treatment", "treatment")) +
+  ylab("average Y") +
+  xlab("period (k)") +
+  theme(panel.grid = element_blank(),
+        legend.title = element_blank())
+
+ggplot(data = dd, aes(x = k, y = y)) +
+  geom_point(aes(color = factor(A, labels = c("Control", "Intervention"))),
+             size = 0.1) +
+  scale_color_manual(values = c("#d07b7c", "#7ba7d0")) +
+  facet_wrap(~site, ncol = 8) +
+  theme(panel.grid = element_blank(),
+        legend.title = element_blank(),
+        axis.text.y = element_text(size = 7),
+        strip.text = element_text(size = 8),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank()) +
+  xlim(c(0, 24)) +
+  guides(color = guide_legend(override.aes = list(size = 2)))
