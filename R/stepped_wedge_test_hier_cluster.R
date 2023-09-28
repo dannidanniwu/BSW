2d495d14a23e30afaa2ae2754baac379b991b6ab
danni <dw2625@nyu.edu>
2023-09-28 14:47:19 -0400

    7. Increase the number of clusters
    1. increase #cluster and sample sizes in each cluster 2. add another model for comparison: linear model with Bayesian basis

--- R/stepped_wedge_test_hier_cluster.R
+++ R/stepped_wedge_test_hier_cluster.R
@@ -32,16 +32,16 @@ s_generate <- function(iter, list_of_defs) {
   list2env(list_of_defs, envir = environment())
   
   #--- add data generation code ---#
-  ds <- genData(10, def, id = "site")#site
-  ds <- addPeriods(ds, 11, "site", perName = "k") #create time periods for each site
+  ds <- genData(30, def, id = "site")#site
+  ds <- addPeriods(ds, 31, "site", perName = "k") #create time periods for each site
   ds <- addColumns(def2, ds)
   #assign the treatment status based on the stepped-wedge design
   #per cluster trt change per wave
-  ds <- trtStepWedge(ds, "site", nWaves = 10, lenWaves = 1, startPer = 1, 
+  ds <- trtStepWedge(ds, "site", nWaves = 30, lenWaves = 1, startPer = 1, 
                      grpName = "A", perName = "k")
   ds$site <- as.factor(ds$site)
   #30 individuals per site per period and generate each individual-level outcome
-  dd <- genCluster(ds, "timeID", numIndsVar = 25, level1ID = "id")
+  dd <- genCluster(ds, "timeID", numIndsVar = 30, level1ID = "id")
   dd <- addColumns(defOut, dd)
   dd[, normk := (k - min(k))/(max(k) - min(k))]#scale time period into range 0-1
   
@@ -52,7 +52,7 @@ s_generate <- function(iter, list_of_defs) {
 s_model <- function(train_data, mod) {
   set_cmdstan_path(path = "/gpfs/share/apps/cmdstan/2.25.0")
   #######Fitting the GAM model with default penalization 
-  fitgam <- bam(y ~ A +  s(k) + s(k, site, bs = "fs"), data = train_data, method="fREML")
+  fitgam <- mgcv::bam(y ~ A +  s(k) + s(k, site, bs = "fs"), data = train_data, method="fREML")
   res_fitgam <- c(summary(fitgam)$p.coeff["A"], summary(fitgam)$se["A"])
   range <-   res_fitgam[1] + c(-1,1) * 1.96 *   res_fitgam[2]
   
@@ -94,22 +94,33 @@ s_model <- function(train_data, mod) {
   #Fit a frequentist linear model with the same basis as the Bayesian model, but no penalization
   # Incorporating the B-spline basis into the data
   ds_with_bspline <- cbind(train_data,  B_train)
+  # Fitting the model using gam
+  bspline_terms <- paste(colnames(B_train), collapse = " + ")
+  formula_str <- paste("y ~ A +", bspline_terms)
+  fitgam3 <- mgcv::bam(as.formula(formula_str), data = ds_with_bspline, method="fREML")
+  res_fitgam3 <- c(summary(fitgam3)$p.coeff["A"], summary(fitgam3)$se["A"])
+  range3 <-   res_fitgam3[1] + c(-1,1) * 1.96 *   res_fitgam3[2]
+  
   
   model_results <- data.table(est_gam_freq= res_fitgam[1], se_gam_freq=res_fitgam[2], 
                               gam_lowci=range[1], gam_upci=range[2], bayes_gam,div, 
                               est_gam_freq_rdn = res_fitgam2[1], se_gam_freq_rdn=res_fitgam2[2], 
                               gam_rdn_lowci=range2[1], gam_rdn_upci=range2[2],
+                              est_gam_freq_np = res_fitgam3[1], se_gam_freq_np=res_fitgam3[2], 
+                              gam_np_lowci=range3[1], gam_np_upci=range3[2],
                               covered_gam_freq=(range[1] < 2 & 2 < range[2]),
-                              covered_bayes,covered_gam_rdn_freq=(range2[1] < 2 & 2 < range2[2])
+                              covered_bayes,covered_gam_rdn_freq=(range2[1] < 2 & 2 < range2[2]),
+                              covered_gam_np_freq=(range3[1] < 2 & 2 < range3[2])
                               ) %>%
-    mutate(across(-c(variable,covered_gam_freq, covered_bayes,covered_gam_rdn_freq), round, 3))
+    mutate(across(-c(variable,covered_gam_freq, covered_bayes,covered_gam_rdn_freq, covered_gam_np_freq), round, 3))
   
   setnames(model_results, c("est_gam_freq","se_gam_freq","lowci_freq", "upci_freq","variable","est_mean_bayes",
                             "est_med_bayes","est_sd_bayes",
                             "lowci_bayes",
                             "upci_bayes","rhat","ess_bulk","ess_tail",
                             "div","est_gam_rdn_freq","se_gam_rdn_freq","lowci_freq_rdn", "upci_freq_rdn",
-                            "covered_freq","covered_bayes","covered_gam_rdn_freq"))
+                            "est_gam_np_freq","se_gam_np_freq","lowci_freq_np", "upci_freq_np",
+                            "covered_freq","covered_bayes","covered_gam_rdn_freq","covered_gam_np_freq"))
   
   return(model_results)
 }
@@ -135,7 +146,7 @@ sjob <- Slurm_lapply(1:100,
                      mod=mod, 
                      njobs = 90, 
                      tmp_path = "/gpfs/data/troxellab/danniw/scratch", 
-                     job_name = "BS_105", 
+                     job_name = "BS_106", 
                      sbatch_opt = list(time = "12:00:00",partition = "cpu_short", `mem-per-cpu` = "8G"), 
                      export = c("s_define","s_generate","s_model","s_single_rep"), 
                      plan = "wait", 
@@ -146,6 +157,6 @@ res <- rbindlist(res) # converting list to data.table
 
 date_stamp <- gsub("-", "", Sys.Date()) 
 dir.create(file.path("/gpfs/data/troxellab/danniw/r/BS/", date_stamp), showWarnings = FALSE) 
-save(res, file = paste0("/gpfs/data/troxellab/danniw/r/BS/", date_stamp, "/stepped_wedge_test_hier_betaA_2.rda"))
+save(res, file = paste0("/gpfs/data/troxellab/danniw/r/BS/", date_stamp, "/stepped_wedge_test_hier_30clusters.rda"))
 
 
