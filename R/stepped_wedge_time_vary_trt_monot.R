28272c56ea2217a4de9b896f67becfcaf2cd95f9
danni <dw2625@nyu.edu>
2023-11-15 16:03:56 -0500

    Final code: Time varying treatment effect: spline model and monotone stepwise function model

---
+++ R/stepped_wedge_time_vary_trt_monot.R
@@ -0,0 +1,183 @@
+library(simstudy)
+library(data.table)
+library(mgcv)
+library(lme4)
+library(splines)
+library(cmdstanr)
+library(brms)
+library(parallel)
+library(dplyr)
+library(slurmR)
+#references:https://www.rdatagen.net/post/2022-12-13-modeling-the-secular-trend-in-a-stepped-wedge-design/
+#references:https://www.rdatagen.net/post/2022-11-01-modeling-secular-trend-in-crt-using-gam/
+set_cmdstan_path(path = "/gpfs/share/apps/cmdstan/2.25.0") 
+# Compile the Stan model
+#monot_mod <- cmdstan_model("./monotone_effect_curve_model.stan")
+monot_mod <- cmdstan_model("/gpfs/home/dw2625/r/BS/monotone_effect_curve_model.stan")
+s_define <- function() {
+  #cluster-specific intercept
+  def <- defData(varname = "a", formula = 0, variance = 0.25)
+  def <- defData(def, varname = "mu_b", formula = 0, dist = "nonrandom")
+  def <- defData(def, varname = "s2_b", formula = 0.36, dist = "nonrandom")
+  #A: trt for each cluster and time period
+  #b: site-specific time period effect
+  #t:exposure time
+  deft <- defDataAdd(varname = "t", formula = "max(k-startTrt,0) ", dist= "nonrandom")
+  
+  defteffect <- defDataAdd(varname = "t_effect", formula = " (0 * (t == 0) +(t!=0)*(..coefA * 1 / (1 + 2*exp(-t))))", dist = "nonrandom")
+  
+  defOut <- defDataAdd(varname = "y", formula = " a + b - 0.05 * k^2 + (0 * (t == 0) +(t != 0)*(..coefA * 1 / (1 + 2*exp(-t))))*A", variance = 1)
+  
+  return(list(def = def, deft=deft, defteffect=defteffect, defOut = defOut)) 
+}
+
+s_generate <- function(iter, coefA, ncluster, list_of_defs) {
+  
+  set.seed(iter)
+  list2env(list_of_defs, envir = environment())
+  
+  #--- add data generation code ---#
+  ds <- genData(ncluster, def, id = "site")#site
+  ds <- addPeriods(ds, ncluster+3+5, "site", perName = "k") #create time periods for each site
+  ds <- addCorGen(
+    dtOld = ds, idvar = "site", 
+    rho = 0.95, corstr = "ar1",
+    dist = "normal", param1 = "mu_b", param2 = "s2_b", cnames = "b"
+  )
+  #assign the treatment status based on the stepped-wedge design
+  #per cluster trt change per wave
+  ds <- trtStepWedge(ds, "site", nWaves = ncluster, lenWaves = 1, startPer = 2, 
+                     grpName = "A", perName = "k")
+  ds$site <- as.factor(ds$site)
+  #30 individuals per site per period and generate each individual-level outcome
+  dd <- genCluster(ds, "timeID", numIndsVar = 10, level1ID = "id")
+  dd <- addColumns(deft, dd)
+  dd <- addColumns(defteffect, dd)
+  dd <- addColumns(defOut, dd)
+  dd[, normk := (k - min(k))/(max(k) - min(k))]#scale time period into range 0-1
+  
+  dd[] #  generated_data is a data.table
+  
+}
+
+s_model <- function(train_data, coefA, monot_mod) {
+  set_cmdstan_path(path = "/gpfs/share/apps/cmdstan/2.25.0")
+
+  t_ex_mx=max(train_data$t)
+  #true value of TATE
+  t_values <- 1:t_ex_mx
+  true_tate = sum(coefA * 1 / (1 + 2*exp(-t_values)))/t_ex_mx
+  #true value of LTE
+  true_lte = coefA * 1 / (1 + 2*exp(-t_ex_mx))
+  ######Bayesian estimation of  TATE & LTE#############
+  knots_train <- quantile(train_data$k, probs=seq(0, 1, length=6)[-c(1, 6)])
+  
+  B_train <- predict(splines::bs(train_data$k, degree=3, knots=quantile(train_data$k, probs=seq(0, 1, length=6)[-c(1, 6)])))
+  
+  #############Bayesian monontone_effect_curve_model#################
+  monot_data <- list(num_data = nrow(train_data),
+                    num_basis = ncol(B_train),
+                    num_t_ex= t_ex_mx,
+                    t_ex = train_data$t,
+                    B = t(B_train),
+                    c=rep(1,t_ex_mx),
+                    A = train_data$A,
+                    y = train_data$y,
+                    num_sites = length(unique(train_data$site)),
+                    site = train_data$site
+                    
+  )
+  fit_monot <- monot_mod$sample(data = monot_data,
+                    refresh = 0, 
+                    iter_warmup = 500,
+                    iter_sampling = 2000,
+                    parallel_chains = 4,
+                    show_messages = TRUE) 
+  
+  monot_diagnostics_df <- as_draws_df(fit_monot$sampler_diagnostics())
+  div_monot <- sum(monot_diagnostics_df[, 'divergent__'])
+  num_max_tree_depth_monot <- sum(monot_diagnostics_df$treedepth__ >= 10)
+  
+  bayes_monot_tate = fit_monot$summary(variables="phi_mean",
+                           posterior::default_summary_measures()[1:3],
+                           quantiles = ~ quantile(., probs = c(0.025, 0.975)),
+                           posterior::default_convergence_measures())
+  covered_bayes_monot_tate =   (bayes_monot_tate$`2.5%`<   true_tate  &   true_tate  < bayes_monot_tate$`97.5%`)
+  
+  
+  bayes_monot_lte = fit_monot$summary(variables="delta",
+                           posterior::default_summary_measures()[1:3],
+                           quantiles = ~ quantile(., probs = c(0.025, 0.975)),
+                           posterior::default_convergence_measures())
+  covered_bayes_monot_lte =   (  bayes_monot_lte$`2.5%`<   true_lte  &   true_lte  <   bayes_monot_lte$`97.5%`)
+  
+  
+  
+  
+  model_results <- data.table(true_tate,true_lte,bayes_monot_tate,bayes_monot_lte,
+                              covered_bayes_monot_tate,covered_bayes_monot_lte,div_monot,
+                              num_max_tree_depth_monot)
+  return(model_results)
+  
+  
+}
+
+s_single_rep <- function(iter, coefA, ncluster, list_of_defs, monot_mod ) {
+  
+  train_data <- s_generate(iter, coefA, ncluster, list_of_defs)
+  
+  model_results <- s_model(train_data, coefA, monot_mod )
+  
+  return(model_results)
+}
+
+s_replicate <- function(iter, coefA, ncluster, monot_mod) {
+  list_of_defs = s_define()
+  model_results = s_single_rep(iter, coefA, ncluster,list_of_defs, monot_mod)
+  return(data.table(iter=iter, coefA = coefA , ncluster=ncluster, model_results))
+}
+
+
+scenarios = expand.grid(coefA= 5,ncluster=10)
+i=1
+nsim=150
+coefA = scenarios[i,"coefA"]
+ncluster= scenarios[i,"ncluster"]
+# res <- replicate(1, s_replicate(iter=1,coefA = coefA,ncluster=ncluster,
+#                                 monot_mod =monot_mod 
+#                                     ))
+# 
+res <- parallel::mclapply(
+    X = 1 : nsim,
+    FUN = function(x) s_replicate(iter=x,coefA = coefA,ncluster=ncluster,
+                                  monot_mod =monot_mod ),
+    mc.cores = 4)
+
+# 
+# 
+# 
+# sjob <- Slurm_lapply(1:nsim,
+#                FUN=s_replicate,
+#                coefA = coefA,
+#                ncluster = ncluster,
+#                monot_mod =monot_mod ,
+#                njobs = 30,
+#                tmp_path = "/gpfs/scratch/dw2625",
+#                job_name = "BS_140",
+#                sbatch_opt = list(time = "24:00:00",partition = "cpu_short", `mem-per-cpu` = "10G"),
+#                export = c("s_define","s_generate","s_model","s_single_rep"),
+#                plan = "wait",
+#                overwrite=TRUE)
+# res <- Slurm_collect(sjob) # data is a list
+# #res<- site_plasma_all[lapply(site_plasma_all, function(x) length(x))>1] #filter out the error message
+# res <- rbindlist(res) # converting list to data.table
+
+date_stamp <- gsub("-", "", Sys.Date())
+dir.create(file.path("/gpfs/home/dw2625/r/BS/", date_stamp), showWarnings = FALSE)
+save(res, file = paste0("/gpfs/home/dw2625/r/BS/", date_stamp, "/monot_time_vary_trt",coefA,"ncluster",ncluster,".rda"))
+
+#result <- cbind(res)
+
+#save(res, file = paste0("./scenarios_fixed_coefA",coefA,"ncluster",ncluster,".rda"))
+
+
---
+++ Stan/monotone_effect_curve_model.stan
@@ -0,0 +1,94 @@
+data {
+  int<lower=0> num_t_ex;   // number of steps in the effect curve
+  int<lower=0> num_data;  // number of observations
+  int num_basis;                    // number of basis
+  matrix[num_basis, num_data] B;    // B-spline basis matrix
+  int<lower=0, upper=1> A[num_data];// binary variable
+  vector[num_data] y;               // response variable
+  int<lower=0, upper=num_t_ex> t_ex[num_data];
+  int<lower=1> num_sites;           // number of sites
+  int site[num_data];               // site id for each observation
+  vector[num_t_ex] c; // fixed constants for the Dirichlet prior
+}
+
+parameters {
+  real delta;                     // trt effect
+  vector[num_basis] mu_a_raw;         // global mean for spline coefficients
+  real<lower=0.01, upper=100> omega; // omega, constrained between 0.01 and 100
+  real<lower=1e-3> sigma_mu_a; // scale for the a coefficients
+  simplex[num_t_ex] alpha_raw;  // raw alpha for the standard Dirichlet prior;the elements are constrained to be non-negative and sum to 1
+  real  beta_0;
+  real<lower=1e-3> sigma_a; 
+  matrix[num_sites, num_basis] a_site_raw;
+  real<lower=1e-3> sigma;
+}
+
+transformed parameters {
+  vector[num_data] Y_hat;
+  matrix[num_sites, num_basis] a_site;
+  vector[num_basis] mu_a;
+  vector<lower=0>[num_t_ex] alpha; // the actual alpha parameters for the model
+  
+  mu_a[1] = mu_a_raw[1];
+   //print(" mu_a[1]: ",  mu_a[1]);
+  for (i in 2:num_basis){
+    mu_a[i] =  mu_a[i-1] + mu_a_raw[i] * sigma_mu_a;
+    //print(" mu_a[", i, "]: ",  mu_a[i]);
+  }
+  
+  for (j in 1:num_basis) {
+    a_site[:,j] =  mu_a[j]+ sigma_a* a_site_raw[:,j];
+    //print(" sigma_a: ",  sigma_a);
+    //print("a_site_raw[", j, "]: ",  a_site_raw[:,j]);  
+    //print("a_site[", j, "]: ",  a_site[:,j]);  
+  }
+  
+  
+  // Scale the standard Dirichlet-distributed vector by omega
+  for (j in 1:(num_t_ex)) {
+    alpha[j] = alpha_raw[j] * omega * c[j];
+  }
+
+  // Ensure alpha sums to 1
+  alpha = alpha / sum(alpha);
+  
+    
+  for (i in 1:num_data) {
+    real mu_n = 0; // Initialize mu_n for each data point
+
+    // Only add alpha[t] to mu_n if t > 0
+    if (t_ex[i] > 0) {
+      for (t in 1:t_ex[i]) {
+        mu_n += alpha[t]; // Add the step function effect
+      }
+    }
+
+    // Compute the predicted value Y_hat for each data point
+    Y_hat[i] = beta_0 + delta * mu_n * A[i] + dot_product(a_site[site[i]], B[:,i]);
+  }
+}
+
+model {
+  // Priors
+  delta ~ normal(0, 5);
+  omega ~ uniform(0.01, 100); // Uniform prior for omega
+  alpha_raw ~ dirichlet(rep_vector(1, num_t_ex)); // Standard Dirichlet prior for raw alpha
+  to_vector(a_site_raw) ~ std_normal();
+  sigma ~ student_t(3, 0, 2.5); 
+  mu_a_raw ~ std_normal();
+  sigma_mu_a ~ std_normal();
+  beta_0 ~ std_normal();
+  sigma_a ~ student_t(3, 0, 2.5);
+   y ~ normal(Y_hat, sigma);
+}
+generated quantities {
+  vector[num_t_ex] phi;
+  real phi_mean;
+  real mu_n_posterior = 0; 
+  
+  for (i in 1:num_t_ex) {
+    mu_n_posterior += alpha[i];
+    phi[i] = mu_n_posterior;
+  }
+   phi_mean = mean(delta * phi);
+}
